d1e052784073cfd87906b0e4516e4c947b5dbcc6
Tomas Delvechio <tomasdelvechio17@gmail.com>
2022-10-23 11:42:14 -0300

    sc es recurso y no se cerraba, se usa try-with
    Es para evitar Memory Leak - Para profundizar leer https://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html

--- src/ar/edu/unlu/mvc/vista/VistaConsola.java
+++ src/ar/edu/unlu/mvc/vista/VistaConsola.java
@@ -24,66 +24,68 @@ public class VistaConsola {
 	}
 
 	public void iniciar() {
-		Scanner sc = new Scanner(System.in);
-		int opcion = 0;
-		while (opcion != 5) {
-			mostrarMenu();
-			opcion = sc.nextInt();
-			switch (opcion) {
-				case 1: {
-					crearTarea();
-					break;
+		try (Scanner sc = new Scanner(System.in)) {
+			int opcion = 0;
+			while (opcion != 5) {
+				mostrarMenu();
+				opcion = sc.nextInt();
+				switch (opcion) {
+					case 1: {
+						crearTarea();
+						break;
+					}
+					case 2: {
+						listarTarea();
+						break;
+					}
+					case 3: {
+						marcarTareaComoCompletada();
+						break;
+					}
+					case 4: {
+						eliminarTarea();
+						break;
+					}
+					default:
+						break;
 				}
-				case 2: {
-					listarTarea();
-					break;
-				}
-				case 3: {
-					marcarTareaComoCompletada();
-					break;
-				}
-				case 4: {
-					eliminarTarea();
-					break;
-				}
-				default:
-					break;
-
 			}
 		}
 	}
 
 	private void crearTarea() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("ingrese un titulo");
-		String titulo = sc.nextLine();
-		System.out.println("ingrese una descripcion");
-		String descripcion = sc.nextLine();
-		controlador.crearTarea(titulo, descripcion);
-		System.out.println("Tarea creada..");
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("ingrese un titulo");
+			String titulo = sc.nextLine();
+			System.out.println("ingrese una descripcion");
+			String descripcion = sc.nextLine();
+			controlador.crearTarea(titulo, descripcion);
+			System.out.println("Tarea creada..");
+		}
 	}
 
 	public void listarTarea() {
 		LinkedList<Tarea> tareas = controlador.obtenerListaDeTarea();
 		for (Tarea t : tareas)
 			this.mostrarTarea(t);
-
 	}
 
 	private void eliminarTarea() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("ingrese el id de la tarea a eliminar");
-		int id = sc.nextInt();
-		controlador.eliminarTarea(id);
-		System.out.println("Tarea eliminada..");
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("ingrese el id de la tarea a eliminar");
+			int id = sc.nextInt();
+			controlador.eliminarTarea(id);
+			System.out.println("Tarea eliminada..");
+		}
 	}
 
 	private void marcarTareaComoCompletada() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("ingrese el id de la tarea a marcar como completa");
-		int id = sc.nextInt();
-		controlador.marcarCompleta(id);
-		System.out.println("Tarea modificada..");
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("ingrese el id de la tarea a marcar como completa");
+			int id = sc.nextInt();
+			controlador.marcarCompleta(id);
+			System.out.println("Tarea modificada..");
+		}
 	}
 
 	public void mostrarTarea(Observado observado) {
